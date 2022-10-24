237cafe03752f7ce25c618c44fbd300a733ba045
Silas Huereca <silashuereca.dev@gmail.com>
2022-10-23 19:59:32 -0500

    feat: add routing, layouts for create/edit modes

--- components/demo-layout/DemoWrapper.vue
+++ components/demo-layout/DemoWrapper.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="h-screen w-full overflow-auto relative">
     <DemoHeader class="bg-white" />
-    <div class="flex w-full h-full pt-16 justify-end">
+    <div class="relative flex w-full h-full pt-16 justify-end">
       <!-- styling form slide over -->
-      <DemoSlideOver />
+      <div v-show="state.mode === 'build'" class="w-full flex h-full w-full">
+        <div class="flex h-full w-full">
+          <div class="p-5">
+            Body Content
+          </div>
+        </div>
+        <div class="p-5">
+          <button v-show="!state.openSideBar" type="button" class="block primary-button whitespace-nowrap" @click="state.openSideBar = true">
+            Style Demo
+          </button>
+        </div>
+        <DemoSlideOver :open="state.openSideBar" @close="state.openSideBar = false" />
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +29,7 @@ export default defineComponent({
     const route = useRoute();
     const state = reactive({
       mode: <string>"build",
+      openSideBar: <boolean>false,
     });
 
     onMounted(() => {
